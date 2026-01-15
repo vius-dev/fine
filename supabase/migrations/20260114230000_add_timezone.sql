aa143d02e282e63d3e649660b011dfdb491e4656
vius-dev <handilocals@protonmail.com>
2026-01-15 23:01:39 +0100

    Delete supabase directory
    deleted migrations

--- supabase/migrations/20260114230000_add_timezone.sql
+++
@@ -1,5 +0,0 @@
--- Add timezone column to users table
-ALTER TABLE public.users ADD COLUMN IF NOT EXISTS timezone TEXT;
-
--- Set default to UTC for existing users
-UPDATE public.users SET timezone = 'UTC' WHERE timezone IS NULL;
